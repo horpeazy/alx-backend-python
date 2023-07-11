e5cf1e6c33ce2a4ec94dfc298674b6c2a90a77a6
horpeazy <iyamuhope.nosa647@gmail.com>
2023-07-11 21:07:29 +0100

    feat: pythpn async

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Contains a function name async_generator that will
+will loop 10 times, each time asynchronously wait 1
+second, then yield a random number between 0 and 10
+"""
+import random
+import asyncio
+
+async def async_generator():
+    """
+    async generator yields 10 random numbers
+
+    Args:
+        None
+    Return:
+        None
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+contains a fucntions names measure_runtime that
+measure the time it takes for async_comprehension to
+run
+"""
+import time
+import asyncio
+
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime():
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    return time.time() - start
